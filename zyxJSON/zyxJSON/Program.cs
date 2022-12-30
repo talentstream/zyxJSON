8716d16d08fd51e68a132228beb19fa49652cdb3
talentstream <447721326@qq.com>
2022-12-30 23:52:08 +0800

    secondtest

--- zyxJSON/zyxJSON/Program.cs
+++ zyxJSON/zyxJSON/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace zyxJSON
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Test!");
         }
     }
 }
